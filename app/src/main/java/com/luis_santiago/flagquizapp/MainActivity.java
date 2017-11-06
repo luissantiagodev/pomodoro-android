63a2f624623f958a8a68326c89922328de781474
Luis Fernando <xvleg3ndaryxv@gmail.com>
2017-11-05 19:31:48 -0600

    new ui implemented

--- app/src/main/java/com/luis_santiago/flagquizapp/MainActivity.java
+++ app/src/main/java/com/luis_santiago/flagquizapp/MainActivity.java
@@ -7,130 +7,61 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.CountDownTimer;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
+import com.github.lzyzsd.circleprogress.ArcProgress;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
-import com.luis_santiago.flagquizapp.ui.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String TAG = MainActivity.class.getSimpleName();
 
-    private TextView mMinutesTextview;
-    private TextView mSecondsTextView;
     private Toolbar mToolbar;
+    private Button mReset;
     private Button mStart;
     private Button mStop;
-    private Button mReset;
-    private boolean isStarted = false;
-    private int minutesToStart = Keys.MINUTE_TO_START;
-    private long currentSecond = 60000;
     private NotificationCompat.Builder notificaionBuilder;
     private Bitmap icon;
     private NotificationManager notificationManager;
     private int currentNotificationID = 0;
-    private boolean isFirstTimePressed = false;
     //This is for ads
     private InterstitialAd interstitialAd;
     private AdView mAdview;
 
-    private CountDownTimer countDownTimer2 = new CountDownTimer(currentSecond, 1000) {
 
-        @Override
-        public void onTick(long millisUntilFinished) {
-            //We rest our currentSecond so we can take track on how many seconds have passed on our UI
-            currentSecond -=1000;
-            mSecondsTextView.setText(String.valueOf(currentSecond / 1000));
-
-        }
-        @Override
-        public void onFinish() {
-            //When the 60 sec have passed we have to rest the current minutes
-            Log.e(TAG, "CURRENT MINUTES: "+minutesToStart);
-            Log.e(TAG, "CURRENT SECONDS: "+currentSecond);
-            if(minutesToStart == 0 || minutesToStart<0 && currentSecond == 1000){
-                Log.e(TAG, "Im inside the condition");
-                setUpNotification();
-                resetMinutes();
-
-            }
-            else{
-                minutesToStart-=1;
-                mSecondsTextView.setText(String.valueOf(currentSecond));
-                //We restart the seconds so we dont have negative values
-                currentSecond = 60000;
-                mMinutesTextview.setText(String.valueOf(minutesToStart));
-                //We start our onTick()
-                start();
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initComponents();
-        mStart.setOnClickListener(this);
-        mStop.setOnClickListener(this);
-        mReset.setOnClickListener(this);
+
+
         setSupportActionBar(mToolbar);
+
         icon = BitmapFactory.decodeResource(this.getResources(),
                 R.drawable.ic_launcher_app);
+
         notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        interstitialAd = new InterstitialAd(this);
-        interstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                // Code to be executed when an ad finishes loading.
-                Log.i("Ads", "onAdLoaded");
-            }
 
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                // Code to be executed when an ad request fails.
-                Log.i("Ads", "onAdFailedToLoad NOW"+ errorCode);
-            }
 
-            @Override
-            public void onAdOpened() {
-                // Code to be executed when the ad is displayed.
-                Log.i("Ads", "onAdOpened");
-            }
-
-            @Override
-            public void onAdLeftApplication() {
-                // Code to be executed when the user has left the app.
-                Log.i("Ads", "onAdLeftApplication");
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Code to be executed when when the interstitial ad is closed.
-                Log.i("Ads", "onAdClosed");
-            }
-        });
-
+        /*interstitialAd = new InterstitialAd(this);
         interstitialAd.setAdUnitId("ca-app-pub-5461480863776866/3346084113");
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice("FA73653EA402CC30D55A5140976DA6C4")
                 .build();
         interstitialAd.loadAd(adRequest);
-        mAdview.loadAd(adRequest);
+        mAdview.loadAd(adRequest);*/
     }
 
     @Override
@@ -144,7 +75,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.settings:{
-                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+
                 break;
             }
         }
@@ -152,12 +83,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initComponents(){
-        mMinutesTextview = (TextView) findViewById(R.id.minutes);
-        mSecondsTextView = (TextView) findViewById(R.id.seconds);
         mToolbar = (Toolbar)findViewById(R.id.toolbar);
-        mStart = (Button) findViewById(R.id.start);
-        mStop = (Button) findViewById(R.id.stop);
-        mReset = (Button) findViewById(R.id.reset);
         mAdview = (AdView) findViewById(R.id.adView);
     }
 
@@ -165,44 +91,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.start:{
-                // call the start method
-                if(!isStarted ){
-                    startTiming();
-                    Log.e(TAG,String.valueOf(interstitialAd.isLoaded()));
-                    if(interstitialAd.isLoaded()){
-                        interstitialAd.show();
-                    }
-                }
+
                 break;
             }
             case R.id.stop:{
                 //call the stop method
-                stop();
+
                 break;
             }
 
             case R.id.reset:{
                 //call the reset method
-                resetMinutes();
+
                 break;
             }
         }
     }
 
-    private void stop(){
-        countDownTimer2.cancel();
-        isStarted = false;
-    }
-
-    private void resetMinutes() {
-        countDownTimer2.cancel();
-        currentSecond = 60000;
-        minutesToStart = Keys.MINUTE_TO_START;
-        mSecondsTextView.setText("00");
-        mMinutesTextview.setText(String.valueOf(minutesToStart));
-        isStarted = false;
-        isFirstTimePressed = false;
-    }
 
     private void setUpNotification(){
         notificaionBuilder = new NotificationCompat.Builder(MainActivity.this)
@@ -231,32 +136,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         notificationManager.notify(notificationId, notification);
     }
 
-    private void startTiming(){
-        Log.e(TAG, "Before:"+minutesToStart);
-        if(!isFirstTimePressed){
-            minutesToStart-=1;
-            Log.e("Main activity", "I'm resting");
-            isFirstTimePressed = true;
-        }
-        Log.e(TAG, "NOW:"+minutesToStart);
-        if(minutesToStart==1){
-            mMinutesTextview.setText("0");
-        }
-        mMinutesTextview.setText(String.valueOf(minutesToStart));
-        isStarted = true;
-        countDownTimer2.start();
-        Keys.UPDATE_PERMISSON = false;
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(Keys.UPDATE_PERMISSON){
-            minutesToStart = Keys.MINUTE_TO_START;
-            mMinutesTextview.setText(String.valueOf(minutesToStart));
-            mSecondsTextView.setText("00");
-            isFirstTimePressed = false;
-        }
         if (mAdview != null) {
             mAdview.resume();
         }
